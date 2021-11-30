eb280340d4e22c550b807336d0bd5d366e476692
mchlltene <tenemchll@gmail.com>
2021-11-30 20:34:01 +0800

    News App

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return(
-    <footer className="py-3 bg-dark">
+    <footer className="py-4 bg-dark">
       <div className="container">
         <p className="m-0 text-center text-white">Copyright &copy; NewsApp 2021</p>
         <p className="m-0 text-center text-white">Tene Michell G 🔥</p>
